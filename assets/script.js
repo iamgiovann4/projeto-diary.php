120d571b894c6d5a48555bb37a45b084ddf9fa0e
Your Name <you@example.com>
2022-11-10 17:05:32 -0300

    comecando

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// function teste() {
+//     alert('Olá, my cat!');
+//     var num = prompt('Quantos anos?');
+//     console.log('Eu tenho ' + num + ' anos!!');
+
+//     var paragrafo = document.createElement('p');
+//     paragrafo.innerHTML= 'Eu tenho ' + num + ' anos!!';
+
+//     var div = document.querySelector('.wrapper');
+//     div.appendChild(paragrafo);  
+// }
+
+// window.onload = function () {
+//     teste();
+// }
